729489b3137c76ff38dc742785cf354297540518
njh99 <whdgh1117@naver.com>
2024-11-19 16:17:00 +0900

    Books 프로그램 insert, update, select, delete  테스트

--- studentTest/src/studentTest/ConnectDB.java
+++ studentTest/src/studentTest/ConnectDB.java
@@ -10,96 +10,46 @@ import java.util.ArrayList;
 public class ConnectDB {
 
 	public static void main(String[] args) {
-		//객체참조변수 선언
+
 		Connection con = null;
 		Statement stmt = null;
-		ResultSet rs= null;
+		ResultSet rs = null;
 		ArrayList<Employees> employeesList = new ArrayList<Employees>();
-		//1. jdbc driver load
-		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			System.out.println("1. 드라이버 적재성공");
-		} catch (ClassNotFoundException e) {
-			System.out.println("1. 드라이버 적재실패"+e.toString());
-			e.printStackTrace();
-		}
-		//2. connection
-		try {
-			con = DriverManager.getConnection("jdbc:oracle:thin:@127.0.0.1:1521/xe","hr","hr");
-			System.out.println("2. 오라클 접속성공");
-		} catch (SQLException e) {
-			System.out.println("2. 오라클 접속실패"+e.toString());
-			e.printStackTrace();
-		}
-		//3. statement(query: C,U,R,d)
+
+		// 1. jdbc driver load 2. connection
+		con = DBConnection.dbCon();
+
+		// 3. statement(query: C,U,R,d)
+		// 4. result set
+		// 5. 데이터 저장 진행
 		try {
 			stmt = con.createStatement();
-			System.out.println("3. Statement 객체생성성공");
-		} catch (SQLException e) {
-			System.out.println("3. Statement 객체생성실패");
-			
-		}
-		//4. result set
-		try {
 			rs = stmt.executeQuery("SELECT * FROM EMPLOYEES");
-			System.out.println("4. ResultSet 객체성공");
-		} catch (SQLException e) {
-			System.out.println("4. ResultSet 객체실패");
-		}
-		//5. 데이터 저장 진행
-	
-		try {
-			while(rs.next()) {
+			while (rs.next()) {
 				int employeeId = rs.getInt("EMPLOYEE_ID");
 				String firstName = rs.getString("FIRST_NAME");
 				int salary = rs.getInt("SALARY");
-				System.out.println(employeeId+"\t"+firstName+"\t"+salary);
+				System.out.println(employeeId + "\t" + firstName + "\t" + salary);
 				Employees employees = new Employees(employeeId, firstName, salary);
 				employeesList.add(employees);
 			}
 		} catch (SQLException e) {
-			System.out.println("5. 출력실패");
+			System.out.println("데이터베이스 실행문 에러"+e.toString());
 		}
-		//데이터 출력
+		
+		//6데이터 출력
 		employeesListPrint(employeesList);
-		
-		//6. close
-		if(con != null) {
-			try {
-				con.close();
-				System.out.println("6. con close 성공");
-			} catch (SQLException e) {
-				System.out.println("6. con close 실패"+e.toString());
-				
-			}
-		}
-		if(stmt != null) {
-			try {
-				stmt.close();
-				System.out.println("5. stmt close 성공");
-			} catch (SQLException e) {
-				System.out.println("5. stmt close 실패"+e.toString());
-				
-			}
-		}
-		
-		if(rs != null) {
-			try {
-				rs.close();
-				System.out.println("5. rs close 성공");
-			} catch (SQLException e) {
-				System.out.println("5. rs close 실패"+e.toString());
-				
-			}
-		}
-		
-	}
 
+		// 7. close
+		DBConnection.dbClose(con, stmt, rs);
+
+	}
+	//테이블 내용 출력
 	private static void employeesListPrint(ArrayList<Employees> employeesList) {
-		for(Employees e:employeesList) {
+		for (Employees e : employeesList) {
 			System.out.println(e.toString());
 		}
-		
+
 	}
 
 }
